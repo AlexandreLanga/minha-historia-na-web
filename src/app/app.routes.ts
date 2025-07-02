00714aaf4175ad6b4b90a6f039b2f0624dc0bc45
Alexandre <alelanga424@hotmail.com>
2025-07-02 01:50:09 -0300

    Estilização da dashboard parte 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Dashboard } from './pages/dashboard/dashboard';
 import { MinhaHistoria } from './pages/minha-historia/minha-historia';
 import { Profissional } from './pages/profissional/profissional';
 import { MaisSobreMim } from './pages/mais-sobre-mim/mais-sobre-mim';
-import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: 'dashboard', component: Dashboard },
