d1a03339a3e70f1efb735a26402185b15485df3c
Alexandre <alelanga424@hotmail.com>
2026-01-14 13:44:39 -0300

    feat(shows): implemented shows session, starting with BMTH

--- src/app/pages/about-me/about-me.ts
+++ src/app/pages/about-me/about-me.ts
@@ -1,14 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { SafeUrlPipe } from '../../safe-url.pipe';
+export interface ShowVideo {
+  id: number;
+  tittle: string;
+  place: string;
+  year: number;
+  plataform: 'youtube' | 'vimeo' | 'local';
+  url: string;
+  description?: string;
+}
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, SafeUrlPipe],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
 })
+
 export class AboutMe {
   constructor(private translate: TranslateService){}
 
@@ -73,6 +84,81 @@ export class AboutMe {
       phrase: "ABOUT_ME.PHRASES.4.PHRASE",
       author: "ABOUT_ME.PHRASES.4.AUTHOR"
     }
+  ];
+
+  videos: ShowVideo[] = [
+    {
+      id: 1,
+      tittle: 'We go one more',
+      place: 'Allianz Parque - SP',
+      year: 2024,
+      plataform: 'youtube',
+      url: 'https://www.youtube.com/embed/ouad00k969U',
+      description: 'I mean, wtf is going on?'
+    },
+    {
+      id: 2,
+      tittle: 'THOOOONE',
+      place: 'Allianz Parque - SP',
+      year: 2024,
+      plataform: 'youtube',
+      url: 'https://www.youtube.com/embed/fLBD_NvZoLg',
+      description: 'A great end, for their biggest show'
+    },
+    {
+      id: 3,
+      tittle: 'GETTING INTO SP',
+      place: 'São Paulo - SP',
+      year: 2024,
+      plataform: 'youtube',
+      url: 'https://www.youtube.com/embed/pi3B62kq5w0',
+      description: 'Airplane landing in São Paulo city'
+    },
+    {
+      id: 4,
+      tittle: 'CHOOSING ANTIVIST',
+      place: 'São Paulo - SP',
+      year: 2024,
+      plataform: 'youtube',
+      url: 'https://www.youtube.com/embed/rPucOxio7iE',
+      description: 'Oliver pulled MCLan and DiFerrero to sing ANTIVIST, they tried'
+    },
+    {
+      id: 5,
+      tittle: 'SENTA',
+      place: 'Allianz Parque - SP',
+      year: 2024,
+      plataform: 'youtube',
+      url: 'https://www.youtube.com/embed/Yp8w3J4P0_0',
+      description: 'We burned a lot in the end, literally'
+    },
+    {
+      id: 6,
+      tittle: 'MANTRA',
+      place: 'Allianz Parque - SP',
+      year: 2024,
+      plataform: 'youtube',
+      url: 'https://www.youtube.com/embed/TkYCCWZ7zV0',
+      description: 'One of my favorites (all are)'
+    },
+    {
+      id: 7,
+      tittle: 'TODO MUNDO PULA',
+      place: 'Allianz Parque - SP',
+      year: 2024,
+      plataform: 'youtube',
+      url: 'https://www.youtube.com/embed/C3kVAGqqkTU',
+      description: 'The vibe of this moment is unforgetable'
+    },
+    {
+      id: 8,
+      tittle: 'DARK TRINITY',
+      place: 'Allianz Parque - SP',
+      year: 2024,
+      plataform: 'youtube',
+      url: 'https://www.youtube.com/embed/C3kVAGqqkTU',
+      description: 'MCLan, DiFerrero and Oliver dicided to sing together'
+    },
   ];
 
   showDescription(desc: string) {
