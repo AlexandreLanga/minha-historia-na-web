51f7a77e04af455b0133e04c133f03de4632be28
Alexandre Langa <alelanga424@hotmail.com>
2026-05-11 18:28:24 -0300

    chore(build): added params to build blog routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,16 @@
+import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { BlogService } from './services/blog.service';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'blog/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const blogService = inject(BlogService);
+      return blogService.getPosts().map(post => ({ slug: post.slug }));
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
